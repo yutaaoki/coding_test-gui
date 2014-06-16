6111cf7b87ac37ef04111e02fa383827940b516f
yuta <y_aoki@ubicast.com>
2014-06-16 02:16:27 -0700

    working test

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -3,13 +3,14 @@
 /* jasmine specs for controllers go here */
 
 describe('TestListCtrl', function(){
+
   beforeEach(module('codingAdminApp'));
 
 
   it('create json data', inject(function($controller) {
     //spec body
-    var scope {},
-      ctrl = = $controller('TestListCtrl', { $scope: scope });
+    var scope = {},
+      ctrl = $controller('TestListCtrl', { $scope: scope });
     expect(scope.tests.length).toBe(2);
   }));
 
